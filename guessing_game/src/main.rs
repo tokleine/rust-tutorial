aad63d2aa737f3d6c39cd59a60850f443317d7d1
Tobias Kleine <tobias.kleine@gmx.net>
2024-10-16 12:43:35 +0000

    fix format

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,32 +1,25 @@
-use std::io::stdin;
 use rand::Rng;
 use std::cmp::Ordering;
+use std::io::stdin;
 
 fn main() {
     println!("Guess the number!");
 
-    
-
-    
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
     println!("The secret number is: {secret_number}");
 
     loop {
-
         let mut guess: String = String::new();
-        
+
         println!("Please input your guess:");
-        stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
+        stdin().read_line(&mut guess).expect("Failed to read line");
         println!("You guessed: {guess}");
 
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
         };
-        
 
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
@@ -35,6 +28,6 @@ fn main() {
                 println!("You win!");
                 break;
             }
-        }        
-    }    
+        }
+    }
 }
