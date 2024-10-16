e42f6f62d8322ebdf7747303350dfa87e1b7d062
Tobias Kleine <tobias.kleine@gmx.net>
2024-10-16 12:41:09 +0000

    simplify game with match and loop

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,41 +1,40 @@
 use std::io::stdin;
-// use rand::Rng;
-use rand_core::{RngCore, OsRng};
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the number!");
 
     
 
-    let mut guess: String = String::new();
-    let mut key = [0u8; 32];
-    OsRng.fill_bytes(&mut key);
-    let secret_number = OsRng.next_u32();
+    
+    let secret_number = rand::thread_rng().gen_range(1..=100);
 
     println!("The secret number is: {secret_number}");
 
-    let mut status: bool = false;
+    loop {
 
-
-    while status == false {
+        let mut guess: String = String::new();
+        
         println!("Please input your guess:");
         stdin()
             .read_line(&mut guess)
             .expect("Failed to read line");
-        println!("You guessed: {}", guess);
+        println!("You guessed: {guess}");
 
-        let numeric_guess: u32 = guess.trim().parse::<u32>().expect("Please type a number!");
-
-        if numeric_guess == secret_number {
-            println!("You win!");
-            status = true;
-        } else {
-            println!("Try again!");
-            guess.clear(); // clear the string to start fresh
-        }
-
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
         
 
-        
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }        
     }    
 }
